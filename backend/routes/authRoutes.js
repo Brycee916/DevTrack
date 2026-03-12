63cd34d88ecf99604ffd6f46f982d80b95a83d61
brycee916 <brycechao27@gmail.com>
2026-03-11 23:10:17 -0700

    created authentication routes

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const pool = require("../db/db");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const router = express.Router();
+
+router.post("/login", (req, res) => {
+
+
+
+});
+
+
+
+module.exports = router;
