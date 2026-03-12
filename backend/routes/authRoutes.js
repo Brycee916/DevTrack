e0a92d48f824b6f3bf75d5ba7fb982768cf33b53
brycee916 <brycechao27@gmail.com>
2026-03-11 23:38:23 -0700

    created register route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,6 +5,41 @@ const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
+router.post("/register", async (req, res) => {
+
+    try{
+        // deconstruct and get the email and password from request body
+        const { email, password } = req.body;
+        if (!email || !password){
+            return res.status(400).json({ error: "Email and password required"});
+        }
+
+        // has the password with a salt of 10
+        const hash = await bcrypt.hash(password, 10);
+
+        // parameterized query to prevent sql injection. these values inserted are treated as string, not sql cmds
+        // returns all columns of inserted row such as id, email, hash, and created at
+        const result = await pool.query(
+            "INSERT INTO users (email, password_hash) VALUES ($1,$2) RETURNING *",
+            [email, hash]
+        );
+
+        if (!result.rows[0]){
+            return res.status(400).json({ error: "Failed to create user"});
+        }
+        
+        //http created status code
+        return res.status(201).json(result.rows[0]);
+        
+    } catch(error) {
+        console.error(error);
+
+        //http server error
+        return res.status(500).json({ error: "Server error" });
+    }
+});
+
+
 router.post("/login", (req, res) => {
 
 
